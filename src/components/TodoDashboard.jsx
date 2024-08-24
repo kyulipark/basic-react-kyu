3a1e475d0f3000049347f6a390d87a022153e0d9
kyulipark <kyuli94p@gmail.com>
2024-08-24 18:18:21 +0900

    🎉 add: styled-components 추가

---
+++ src/components/TodoDashboard.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+const TodoDashboard = () => {
+  return (
+    <DashboardSection>
+      <DashboardHeader>
+        <h1>Todo Dashboard</h1>
+      </DashboardHeader>
+      <DashboardCardList>
+        <DashboardCard>16 New Task</DashboardCard>
+        <DashboardCard>3 completed</DashboardCard>
+        <DashboardCard>14 pending</DashboardCard>
+      </DashboardCardList>
+    </DashboardSection>
+  );
+};
+
+export default TodoDashboard;
+
+const DashboardSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const DashboardHeader = styled.div`
+  h1 {
+    font-size: 1rem;
+  }
+`;
+
+const DashboardCardList = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+const DashboardCard = styled.div`
+  background-color: orange;
+  padding: 8px;
+  border-radius: 1rem;
+  width: 120px;
+  height: 120px;
+
+  color: white;
+  font-weight: bold;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+
+  &:first-child {
+    width: 240px;
+  }
+`;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TodoItem = ({ todo, handleUpdate, handleDelete }) => {
   return (
     <li key={todo.id}>
