0bc7039942fdbfb00e92d7b9408aa67fe9769492
kyulipark <kyuli94p@gmail.com>
2024-09-05 22:13:55 +0900

    add: layout 생성

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-
-const SAMPLE_TODOS = [
-  { id: 1, text: "Buy milk" },
-  { id: 2, text: "Clean the house" },
-  { id: 3, text: "Go for a run" },
-  { id: 4, text: "Finish homework" },
-  { id: 5, text: "Call mom" },
-  { id: 6, text: "Buy groceries" },
-  { id: 7, text: "Walk the dog" },
-  { id: 8, text: "Read a book" },
-  { id: 9, text: "Do laundry" },
-  { id: 10, text: "Write code" },
-];
-const TodoList = () => {
-  const [todos, setTodos] = useState(SAMPLE_TODOS);
-  const [newTodo, setNewTodo] = useState("");
-
-  const handleSubmit = (e) => {
-    //새로고침 방지
-    e.preventDefault();
-    //유효성검사
-    if (!newTodo.trim()) {
-      return;
-    }
-    setTodos([...todos, { id: crypto.randomUUID(), text: newTodo }]);
-    setNewTodo("");
-  };
-
-  const handleInputChange = (e) => setNewTodo(e.target.value);
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="todo"
-          value={newTodo}
-          onChange={handleInputChange}
-          placeholder="Todo를 추가해주세요!"
-        />
-        <button type="submit">추가하기</button>
-      </form>
-      <ul>
-        {todos.map((newTodo) => (
-          <li key={newTodo.id}>{newTodo.text}</li>
-        ))}
-      </ul>
-    </div>
-=======
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
 
@@ -66,7 +15,6 @@ const TodoList = ({ todos, handleUpdate, handleDelete }) => {
         ))}
       </ul>
     </StyledUI>
->>>>>>> 23be3e73436abc24f3cc9e9a9a30337d556dc4ef
   );
 };
 
